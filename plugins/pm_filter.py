c37c2f048937f22a5c06b06b3a02dd9cb9a4cb2c
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2023-12-11 10:25:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -114,7 +114,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⚡️{get_size(file.file_size)}⚡️{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                    text=f"👉{get_size(file.file_size)}⚡️{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
             ]
             for file in files
@@ -136,7 +136,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⚡️{get_size(file.file_size)}⚡️{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"👉{get_size(file.file_size)}⚡️{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1885,7 +1885,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⚡️{get_size(file.file_size)}⚡️{file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                    text=f"👉{get_size(file.file_size)}⚡️{file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
             ]
             for file in files
@@ -1908,7 +1908,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⚡️{get_size(file.file_size)}⚡️{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"👉{get_size(file.file_size)}⚡️{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
