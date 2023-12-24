15276b2089b7f7b1b046929949df1db55159f607
tnviper33 <119023924+tnviper33@users.noreply.github.com>
2023-12-24 08:14:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), True)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://think:think@cluster015.9oqeden.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster015")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://arun:<password>@cluster089.ilwjafv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster089")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ipopuding')
 
 # Others
